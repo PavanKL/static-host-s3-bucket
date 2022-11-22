d433366c7d848c201eeec163697e524c505521e4
PavanKL <47595732+PavanKL@users.noreply.github.com>
2022-11-22 09:03:12 +0530

    Update hosted-zone.tf

--- hosted-zone.tf
+++ hosted-zone.tf
@@ -9,19 +9,10 @@ resource "aws_route53_record" "exampleDomain-a" {
   name    = var.domainName
   type    = "A"
   alias {
-    name                   = aws_s3_bucket_website_configuration.elsevier-config.website_endpoint
+    name                   = aws_s3_bucket.elsevier.website_endpoint
     zone_id                = aws_s3_bucket.elsevier.hosted_zone_id
     evaluate_target_health = true
   }
 }
 
-resource "aws_route53_record" "CNAME" {
-
-  zone_id = aws_route53_zone.main.zone_id
-  name    = "www.${var.domainName}"
-  type    = "CNAME"
-  ttl     = "10"
-  records = [aws_s3_bucket_website_configuration.elsevier-config.website_endpoint]
-
-}
 
